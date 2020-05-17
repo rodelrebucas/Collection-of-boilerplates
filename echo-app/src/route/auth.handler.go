b001e1572e0b9f6da35f7bb46188a672cbb4f5fe
rodel <rodelrebucas@gmail.com>
2020-05-18 00:39:57 +0800

    echo auth handler

--- echo-app/src/route/auth.handler.go
+++ echo-app/src/route/auth.handler.go
@@ -4,13 +4,11 @@ import (
 	"os"
 	"net/http"
 	"time"
-
 	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
 
-// Auth godoc
+// AuthenticateHandler - handles /authenticate route
 // @tags Auth
 // @Summary Creates token for subscribers
 // @Description TODO
@@ -19,68 +17,52 @@ import (
 // @Param id formData string true "id of connecting client"
 // @Header 200 {string} Token
 // @Router /authenticate [post]
-// AuthenticateHandler - handles /authenticate route
 func AuthenticateHandler(c echo.Context) (err error) {
-		user := createUser()
-		if err = c.Bind(user); err != nil {
-			return
-		}
-		if err = user.validate(); err != nil {
-			var errs string
-			for _, errss := range err.(validator.ValidationErrors) {
-				errs = errs + user.err[errss.Field()] + "\n"
-			}
-			error404 := &errorUser{
-				Code: 404,
-				Msg:  errs,
-			}
-			return c.JSON(http.StatusBadRequest, error404)
-		}
-		tokenString := CreateToken(user)
+	errorRes := &ErrorUser{
+		Status: 404,
+		Msg:  "",
+	}
+	user := &GenericUser{
+		err: map[string]string{
+			"ID": "name - is required and length greater than 5",
+		},
+	}
+	if err = c.Bind(user); err != nil {
+		return
+	}
+	if err = Validate(user); err != nil {
+		errorRes.Msg = ExtractError(user, err)
+		return c.JSON(http.StatusNotFound, errorRes)
+	}
+	if CheckExistingUser(user) {
+		tokenString := CreateToken(user.ID, os.Getenv("SECRET_KEY"))
 		return c.JSON(http.StatusOK, struct {
 			Token string `json:"token"`
 		}{
 			Token: tokenString,
 		})
+	}
+	errorRes.Msg = "User Not Found!"
+	errorRes.Status = http.StatusBadRequest
+	return c.JSON(http.StatusBadRequest, errorRes)
+	
+}
+
+// CheckUser - check for existing users
+func CheckExistingUser(u *GenericUser) bool {
+	// TODO: Add user retrieval logic
+	if u.ID == os.Getenv("KNOWN_USER") {
+		return true
+	} else { return false }
 }
 
 // CreateToken - creates the token for the specified user
-func CreateToken(u *user) (tokenString string) {
+func CreateToken(id string, secret string) (tokenString string) {
 	timeNow := time.Now()
 	timeNowUTC := timeNow.UTC()
-	// finalize token props
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": u.ID,
-		"exp": timeNowUTC.Add(time.Minute * 30)})
-	tokenString, _ = token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+		"sub": id,
+		"exp": timeNowUTC.Add(time.Minute * 30).Unix()})
+	tokenString, _ = token.SignedString([]byte(secret))
 	return
-}
-
-var validate *validator.Validate
-
-// Error generic error return type
-type errorUser struct {
-	Code int    `json:"code" xml: "code"`
-	Msg  string `json:"msg" xml: "msg"`
-}
-
-type user struct {
-	ID  string `json:"name" validate:"required,gt=5"`
-	err map[string]string
-}
-
-func (u *user) validate() (err error) {
-	validate = validator.New()
-	if err = validate.Struct(u); err != nil {
-		return
-	}
-	return nil
-}
-
-func createUser() *user {
-	return &user{
-		err: map[string]string{
-			"ID": "name - is required and length greater than 5",
-		},
-	}
 }
