790783a2c9ddd243b795b62e0a5583ddd65e865c
rodel <rodelrebucas@gmail.com>
2020-09-28 19:03:36 +0800

    update index

--- express-app/src/index.js
+++ express-app/src/index.js
@@ -2,10 +2,9 @@ import { port, env } from "./config/vars";
 import logger from "./config/logger";
 import app from "./config/express";
 
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== "test")
   app.listen(port, () => {
     logger.info(`Server started on port ${port} (${env})`);
   });
-}
 
 module.exports = app;
